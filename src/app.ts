f53a93b02eaadf3cbe2127737497a30a2dae8162
jsakamoto <junichi.jp@gmail.com>
2017-12-14 22:10:27 +0900

    initial commit.

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import * as $ from 'jquery'
+
+$(() => {
+    let randomNum = Math.round(100 * Math.random())
+    $('#p1').text(randomNum)
+})
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: ['./src/app.ts'],
+    output: {
+        filename: './wwwroot/js/bundle.js'
+    },
+    resolve: { extensions: ['.js', '.ts'] },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/, use: [
+                    { loader: 'ts-loader' }
+                ]
+            }
+        ]
+    },
+    devtool: 'source-map'
+};
